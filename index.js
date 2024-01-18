dcee7c36e264609f5f2c2a7328d0592491a92289
abdul salaam <salaam.m@Technovert.com>
2024-01-18 14:50:21 +0530

    Modified welcome message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port=3000
 
 app.get('/',(req,res)=>{
-    res.status(200).send('Please join todays party!! I cordially invite you to join us !!, Will be waiting for your presence!!')
+    res.status(200).send(' I cordially invite you to join us !!,Please join todays party!!, Will be waiting for your presence!!')
 })
 
 app.listen(port,()=>{
