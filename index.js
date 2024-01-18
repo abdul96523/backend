76667cb9f4429877b153d664d2cbdd8e818a1db9
abdul salaam <salaam.m@Technovert.com>
2024-01-18 09:38:56 +0530

    Chnaged message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port=3000
 
 app.get('/',(req,res)=>{
-    res.status(200).send('Get request from user')
+    res.status(200).send('Hello guys')
 })
 
 app.listen(port,()=>{
